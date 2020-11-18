e9a0b063c4cd570e74c560096757a0bf7ccb1d76
abdullah-mohammed <aimohammed@ucdavis.edu>
2020-11-18 03:28:59 -0800

    comment in index

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap';
 import 'react-scrollable-anchor';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+//random commit to update
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
